32ace182c3d85de1710f1c2982deb1288b9cebae
NjimsRane <njims2011@gmail.com>
2022-10-27 16:55:17 +0100

    resized few things

--- App/JS/srcipt.js
+++ App/JS/srcipt.js
@@ -1,7 +1,9 @@
 const preloader = document.querySelector(".preloader");
+const arrow = document.querySelector(".arrow");
 
 // hide the preloader
 function show(){
     preloader.classList.add("preloader-hidden");
+    arrow.classList.add("arrow-hide");
 };
 setInterval(show,2000)
